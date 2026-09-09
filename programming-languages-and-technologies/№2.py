7bcfb29605e8c56a6fc8dd093b5a46c0c813bb08
Madiyarrrrrrr <zantleuoamadiar@gmail.com>
2026-09-09 14:14:35 +0500

    *

--- programming-languages-and-technologies/№2.py
+++
@@ -1,8 +0,0 @@
-length = float(input("Введите длину: "))
-width = float(input("Введите ширину: "))
-
-area = length * width
-perimeter = 2 * (length + width)
-
-print("Площадь:", area)
-print("Периметр:", perimeter)
